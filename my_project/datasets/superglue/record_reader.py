2be5d46ee5567bcc3855181101c6200df969ea7d
Gabe <gabeorlanski@gmail.com>
2021-03-12 16:05:23 -0500

    Added Span Extraction for Entities
    Added span extraction for entities to _read. Still need to do more

--- my_project/datasets/superglue/record_reader.py
+++ my_project/datasets/superglue/record_reader.py
@@ -21,6 +21,7 @@ from allennlp.data.fields import TextField, SequenceLabelField, Field, MetadataF
 from allennlp.data.instance import Instance
 from allennlp.data.token_indexers import TokenIndexer, SingleIdTokenIndexer
 from allennlp.data.tokenizers import Token, Tokenizer, SpacyTokenizer
+import json
 
 logger = logging.getLogger(__name__)
 
@@ -35,39 +36,118 @@ class RecordTaskReader(DatasetReader):
     Reading Comprehension (arxiv.org/pdf/1810.12885.pdf) by Zhang et al. Leaderboards and the
     official evaluation script for the ReCoRD task can be found sheng-z.github.io/ReCoRD-explorer/.
 
-    The reader reads a JSON file in the format from sheng-z.github.io/ReCoRD-explorer/dataset-readme.txt
+    The reader reads a JSON file in the format from
+    sheng-z.github.io/ReCoRD-explorer/dataset-readme.txt
+    
 
     # Parameters
 
-    tokenizer: `Tokenizer`
+    tokenizer: `Tokenizer`, optional
         The tokenizer class to use. Defaults to SpacyTokenizer
+
+    token_indexers : `Dict[str, TokenIndexer]`, optional
+        We similarly use this for both the question and the passage.  See :class:`TokenIndexer`.
+        Default is `{"tokens": SingleIdTokenIndexer()}`.
+
+    passage_length_limit : `int`, optional (default=`None`)
+        If specified, we will cut the passage if the length of passage exceeds this limit.
+
+    question_length_limit : `int`, optional (default=`None`)
+        If specified, we will cut the question if the length of question exceeds this limit.
+
+    raise_errors: `bool`, optional (default=`False`)
+        If the reader should raise errors or just continue.
 
     kwargs: `Dict`
         Keyword arguments to be passed to the DatasetReader parent class constructor.
 
     """
 
-    def __init__(self, tokenizer: Tokenizer = None, **kwargs) -> None:
+    def __init__(self,
+                 tokenizer: Tokenizer = None,
+                 token_indexers: Dict[str, TokenIndexer] = None,
+                 passage_length_limit: int = None,
+                 question_length_limit: int = None,
+                 raise_errors: bool = False,
+                 **kwargs) -> None:
         """
         Initialize the RecordTaskReader.
         """
         super(RecordTaskReader, self).__init__(**kwargs)
 
-        # Load either the passed tokenizer or initialize a spacy tokenizer.
+        # Save the values passed to __init__ to protected attributes
         self._tokenizer = tokenizer or SpacyTokenizer()
+        self._token_indexers = token_indexers or {"tokens": SingleIdTokenIndexer()}
+        self._passage_length_limit = passage_length_limit
+        self._question_length_limit = question_length_limit
+        self._raise_errors = raise_errors
 
     @overrides
     def _read(self, file_path: Union[Path, str]) -> Iterable[Instance]:
-        raise NotImplementedError("'RecordTaskReader._read' is not yet implemented")
+        # IF `file_path` is a URL, redirect to the cache
+        file_path = cached_path(file_path)
 
+        # Read the 'data' key from the dataset
+        logger.info(f"Reading '{file_path}'")
+        with open(file_path) as fp:
+            dataset = json.load(fp)['data']
+        logger.info(f"Found {len(dataset)} examples from '{file_path}'")
+
+        # Iterate through every example from the ReCoRD data file.
+        for example in dataset:
+            example: Dict
+            # Get the data from the example dict. Wrap in a Try block in order
+            # to log any errors that come up. If raise_errors is enabled, it
+            # will raise them.
+            try:
+                # Get the passage dict from the example, it has text and entities
+                passage_dict: Dict = example['passage']
+                passage_text: str = passage_dict['text']
+
+                # Entities are stored as a dict with the keys 'start' and 'end' for
+                # their respective char indices.
+                passage_entities = [self.get_span_from_text(passage_text, e['start'], e['end'])
+                                    for e in passage_dict['entities']]
+                logger.debug(f"Found {len(passage_entities)} entities in {example['id']}")
+
+                # Get the queries from the example dict
+                queries: List = example['qas']
+            except KeyError as e:
+                logger.error(f"{example['id']} raised error '{e}'")
+                if self._raise_errors:
+                    raise e
+                continue
+
+
+    @staticmethod
+    def get_span_from_text(text: str,
+                           start: int,
+                           end: int) -> str:
+        """
+        Helper function to get a span from a string
+
+        Args:
+            text: `str`
+                The source string
+            start: `int`
+                The starting index
+            end: `int`
+                The end index. It is INCLUSIVE.
+
+        Returns: `str`
+            The extracted string from text.
+
+        """
+        return ''.join(text[start:end])
+
+    @overrides
     def text_to_instance(
             self,
             passage: List[str],
     ) -> Instance:
-
         """
         TODO: Needs to Return
-            question_with_context: Dict[str, Dict[str, torch.LongTensor]],
+            question_with_context: Dict[str, Dict[str, torch.LongTensor]]
             context_span: torch.IntTensor
             cls_index: Optional[torch.LongTensor]
             answer_span: Optional[torch.IntTensor]
